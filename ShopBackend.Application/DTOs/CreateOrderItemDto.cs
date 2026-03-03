42669c19a7cb98d2b1faf0d38d12206b49ba93c3
Sedlak S. <stefan_sedlak@yahoo.de>
2026-03-03 14:49:29 +0100

    Add Application Layer - Interfaces und DTOs 1.1

--- ShopBackend.Application/DTOs/CreateOrderItemDto.cs
+++ ShopBackend.Application/DTOs/CreateOrderItemDto.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using ShopBackend.Domain.Entities;
+
 
 namespace ShopBackend.Application.DTOs
 {
     public class CreateOrderItemDto
     {
+        // Keine OrderItemId, da diese automatisch von der Datenbank generiert wird, wenn eine neue OrderItem-Entität erstellt wird, sie existiert jetzt ja noch nicht.
         public int ProductId { get; set; } 
         public int Quantity { get; set; }
     }
