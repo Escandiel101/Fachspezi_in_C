a76b718260309dd4bb5d062e231bf44d09487fba
Sedlak S. <stefan_sedlak@yahoo.de>
2026-03-04 12:29:59 +0100

    Add - UserService.cs to Infrastructure

---
+++ ShopBackend.Infrastructure/Services/UserService.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ShopBackend.Application.DTOs;
+using ShopBackend.Application.Interfaces;
+using ShopBackend.Domain.Entities;
+using ShopBackend.Infrastructure.Data;
+using System;
+using System.ComponentModel.Design;
+
+
+namespace ShopBackend.Infrastructure.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext _context;
+
+        public UserService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public async Task ChangePasswordAsync(int id, ChangePasswordDto dto)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                throw new KeyNotFoundException($"User mit der ID: {id} nicht gefunden.");
+
+           if (user.PasswordHash != dto.CurrentPassword) 
+                throw new UnauthorizedAccessException("Falsches Passwort"); // Generische Antworten sind besser als Detailreiche. So weiß ein pot. Angreifer nicht was genau falsch ist. 
+
+            user.PasswordHash = dto.NewPassword; // Hashing kommt später
+                await _context.SaveChangesAsync();
+        }
+
+
+        public async Task<User> CreateAsync(CreateUserDto dto)
+        {
+            var user = new User
+            {
+                Email = dto.Email,
+                PasswordHash = dto.Password, // Hashing kommt später
+                Role = "Customer",
+                CreatedAt = DateTime.UtcNow,
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+
+
+            /* TypenUNsicherer, Fehleranfälliger... länger auch so möglich: 
+            await _context.Database.ExecuteSqlRawAsync(
+            "INSERT INTO Users (Email, PasswordHash, Role, CreatedAt) VALUES ({0}, {1}, {2}, {3})",
+            dto.Email, dto.Password, "Customer", DateTime.UtcNow);
+            */
+        }
+
+
+        public async Task DeleteAsync(int id)
+        {
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                throw new KeyNotFoundException($"User mit der ID: {id} nicht gefunden.");
+
+            // Man braucht hier keine else oder else if, da die Methode nach dem Throwen der Exception sowieso abgebrochen wird.
+            // Es ist also nicht möglich, dass der Code weiterläuft, wenn der User nicht gefunden wurde.
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            
+        }
+
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+
+        }
+
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null) 
+                throw new KeyNotFoundException($"User mit der ID: {id} nicht gefunden.");
+            return user;
+        }
+
+
+        public async Task UpdateAsync(int id, UpdateUserDto dto)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                throw new KeyNotFoundException($"User mit der ID: {id} nicht gefunden.");
+            
+            user.Email = dto.Email ?? user.Email;
+            // man könnte auch händischer: if (dto.Email != null) user.Email = dto.Email; machen, aber so ist es kürzer und funktioniert genauso gut
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
