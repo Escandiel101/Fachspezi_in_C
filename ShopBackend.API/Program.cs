797bf24b9aca8f65055c9eb79fac42c164639faf
Sedlak S. <stefan_sedlak@yahoo.de>
2026-03-23 11:50:43 +0100

    Updated program.cs, secret key from appsettings = dummy key only, several structure updates for Swagger functionality, started testing, updated userService for password security

--- ShopBackend.API/Program.cs
+++ ShopBackend.API/Program.cs
@@ -5,10 +5,13 @@ using Microsoft.IdentityModel.Tokens;
 using ShopBackend.API.Middleware;
 using ShopBackend.Application.Authorization;
 using ShopBackend.Application.Interfaces;
+using ShopBackend.Domain.Entities;
 using ShopBackend.Infrastructure.BackgroundServices;
 using ShopBackend.Infrastructure.Data;
 using ShopBackend.Infrastructure.Services;
 using System.Text;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -49,6 +52,7 @@ builder.Services.AddHostedService<IdleUserDeactivator>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
+
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,              // Kommt der Token überhaupt von meinem Server?
@@ -90,18 +94,85 @@ builder.Services.AddAuthorization(options =>        // Policy Optionen für die 
 
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
+
+// Swagger Test Tool mit UI einfügen:
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,   // wichtig
+        Scheme = "bearer",               
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter: Bearer {your token}"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 
 // Alle builder. Parameter müssen vor dem hier stehen, sonst werden sie nicht "gebaut"
 var app = builder.Build();
 
+// legt einen Admin an, sonst wird das ganze etwas kniffelig, wenn nur ein Admin einen anderen Admin ernennen kann.
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    if (!context.Users.Any())
+    {
+        context.Users.Add(new User
+        {
+            Email = "admin@shop.de",
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword("Admin123!"),
+            Role = UserRole.Admin,
+            CreatedAt = DateTime.UtcNow
+        });
+        await context.SaveChangesAsync();
+    }
+}
+// muss vor CORS stehen...
+app.UseHttpsRedirection();
+// Muss vor der Swagger Pipeline stehen, sonst kann CORS (Cross-Origin Resource Sharing nicht kommunizieren und dann lehnt es mir im Swagger den Zugriff ab.
+app.UseCors("AllowAll");
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    // Das hier generiert die Interface-Seite für Swagger
+    app.UseSwaggerUI(c => 
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shop API v1");
+        c.RoutePrefix = string.Empty; // optional: Swagger direkt auf 
+    }); 
 }
 
-app.UseHttpsRedirection();
+
 
 app.UseMiddleware<ExceptionMiddleware>();
 
